333b67787dd4d325e16a4e366074fd487801e888
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-10-29 17:15:11 -0500

    Modelo de reporte de incidencia

---
+++ models/incident_report.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+from models import Channel
+
+
+@dataclass
+class Incident:
+    client_id: str
+    name: str
+    channel: Channel
+    reported_by: str
+    created_by: str
+    description: str
