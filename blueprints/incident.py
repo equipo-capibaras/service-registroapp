31c96b67798bdd9ddcbd1e508bd8b2054aadd7b7
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-10-29 17:27:55 -0500

    Clase de validación de incidentes

--- blueprints/incident.py
+++ blueprints/incident.py
@@ -1,17 +1,35 @@
+from dataclasses import field, dataclass
 from typing import Any
 
+import marshmallow
 from dependency_injector.wiring import Provide
 from flask import Blueprint, Response
 from flask.views import MethodView
 
 from containers import Container
-from models import Channel
+from models import Channel, Incident
 from repositories import IncidentRepository
 
 from .util import class_route, json_response, requires_token
 
 blp = Blueprint('Incidents', __name__)
 
+def incident_to_dict(incident: Incident) -> dict[str, Any]:
+    return {
+        'client_id': incident.client_id,
+        'name': incident.name,
+        'channel': incident.channel.value,
+        'reported_by': incident.reported_by,
+        'created_by': incident.created_by,
+        'description': incident.description,
+    }
+
+# Incident validation class
+@dataclass
+class IncidentRegistrationBody:
+    email: str = field(metadata={'validate': [marshmallow.validate.Email(), marshmallow.validate.Length(min=1, max=60)]})
+    name: str = field(metadata={'validate': [marshmallow.validate.Length(min=1, max=90)]})
+    description: str = field(metadata={'validate': [marshmallow.validate.Length(min=1, max=5000)]})
 
 @class_route(blp, '/api/v1/users/me/incidents')
 class UserIncidents(MethodView):
@@ -33,3 +51,4 @@ class UserIncidents(MethodView):
         )
 
         return json_response({'id': '753f5554-c545-447d-8a4d-4eccda9e952a'}, 201)
+
--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from .channel import Channel
+from .incident_report import Incident
 from .role import Role
 from .user import User
 
-__all__ = ['Channel', 'Role', 'User']
+__all__ = ['Channel', 'Role', 'User', 'Incident']
