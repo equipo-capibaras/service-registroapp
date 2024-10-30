f654e820dbc1d86ede6ac189ba95a614609183ea
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-10-29 22:04:21 -0500

    Consultar usuario por email en repositories

--- repositories/user.py
+++ repositories/user.py
@@ -4,3 +4,6 @@ from models import User
 class UserRepository:
     def get(self, user_id: str, client_id: str) -> User | None:
         raise NotImplementedError  # pragma: no cover
+
+    def find_by_email(self, email: str) -> User | None:
+        raise NotImplementedError  # pragma: no cover
