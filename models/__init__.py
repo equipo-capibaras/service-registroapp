bf0fac5c513fbfed1bbc7034912e66280b94d4e9
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-10-31 16:32:58 -0500

    No se necesita empleado

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 from .channel import Channel
-from .employee import Employee
 from .incident_report import Incident
 from .role import Role
 from .user import User
 
-__all__ = ['Channel', 'Role', 'User', 'Incident', 'Employee']
+__all__ = ['Channel', 'Role', 'User', 'Incident']
