15875f5414fc09d521e609425a6562b0a80826e7
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-10-31 21:47:42 -0500

    Se corrigen test de endpoint

--- tests/blueprints/test_incident.py
+++ tests/blueprints/test_incident.py
@@ -8,7 +8,7 @@ from unittest_parametrize import ParametrizedTestCase, parametrize
 from werkzeug.test import TestResponse
 
 from app import create_app
-from models import Channel, Role, User
+from models import Channel, Role, User, IncidentResponse
 from repositories import IncidentRepository, UserRepository
 
 from .util import gen_token
@@ -84,142 +84,155 @@ class TestIncident(ParametrizedTestCase):
 
         self.assertEqual(resp.status_code, 201)
 
-    # def test_web_incident_no_token(self) -> None:
-    #     resp = self.call_web_incident_api(None, None)
-    #     self.assertEqual(resp.status_code, 401)
-    #     resp_data = json.loads(resp.get_data())
-    #     self.assertEqual(resp_data, {'code': 401, 'message': 'Token is missing'})
-    #
-    # @parametrize(
-    #     'role',
-    #     [
-    #         (Role.USER,),
-    #         (Role.ANALYST,),
-    #     ],
-    # )
-    # def test_web_incident_invalid_role(self, role: Role) -> None:
-    #     token = gen_token(
-    #         user_id=cast(str, self.faker.uuid4()),
-    #         client_id=cast(str, self.faker.uuid4()),
-    #         role=role,
-    #         assigned=True,
-    #     )
-    #     resp = self.call_web_incident_api(token, {})
-    #     self.assertEqual(resp.status_code, 403)
-    #     resp_data = json.loads(resp.get_data())
-    #     self.assertEqual(resp_data, {'code': 403, 'message': 'Forbidden: You do not have access to this resource.'})
-    #
-    # def test_web_incident_no_client(self) -> None:
-    #     token = gen_token(
-    #         user_id=cast(str, self.faker.uuid4()),
-    #         client_id=None,
-    #         role=Role.ADMIN,
-    #         assigned=True,
-    #     )
-    #     resp = self.call_web_incident_api(token, {})
-    #     self.assertEqual(resp.status_code, 401)
-    #     resp_data = json.loads(resp.get_data())
-    #     self.assertEqual(resp_data, {'code': 401, 'message': 'Unauthorized: You do not belong to any client.'})
-    #
-    # def test_web_incident_invalid_body(self) -> None:
-    #     token = gen_token(
-    #         user_id=cast(str, self.faker.uuid4()),
-    #         client_id=cast(str, self.faker.uuid4()),
-    #         role=Role.ADMIN,
-    #         assigned=True,
-    #     )
-    #     resp = self.call_web_incident_api(token, {'name': 'test'})
-    #     self.assertEqual(resp.status_code, 400)
-    #     resp_data = json.loads(resp.get_data())
-    #     self.assertEqual(resp_data['code'], 400)
-    #
-    # def test_web_incident_user_not_found(self) -> None:
-    #     token = gen_token(
-    #         user_id=cast(str, self.faker.uuid4()),
-    #         client_id=cast(str, self.faker.uuid4()),
-    #         role=Role.ADMIN,
-    #         assigned=True,
-    #     )
-    #     body = {
-    #         'email': self.faker.email(),
-    #         'name': self.faker.word(),
-    #         'description': self.faker.sentence(),
-    #     }
-    #
-    #     user_repo_mock = Mock(UserRepository)
-    #     cast(Mock, user_repo_mock.find_by_email).return_value = None
-    #
-    #     with self.app.container.user_repo.override(user_repo_mock):
-    #         resp = self.call_web_incident_api(token, body)
-    #
-    #     self.assertEqual(resp.status_code, 404)
-    #     resp_data = json.loads(resp.get_data())
-    #     self.assertEqual(resp_data['code'], 404)
-    #     self.assertEqual(resp_data['message'], 'Invalid value for email: User does not exist.')
-    #
-    # def test_web_incident_user_not_belonging_to_client(self) -> None:
-    #     token = gen_token(
-    #         user_id=cast(str, self.faker.uuid4()),
-    #         client_id=cast(str, self.faker.uuid4()),
-    #         role=Role.ADMIN,
-    #         assigned=True,
-    #     )
-    #     user = User(
-    #         id=cast(str, self.faker.uuid4()),
-    #         client_id=cast(str, self.faker.uuid4()),  # Different client_id than token
-    #         name=self.faker.name(),
-    #         email=self.faker.email(),
-    #     )
-    #     body = {
-    #         'email': user.email,
-    #         'name': self.faker.word(),
-    #         'description': self.faker.sentence(),
-    #     }
-    #
-    #     user_repo_mock = Mock(UserRepository)
-    #     cast(Mock, user_repo_mock.find_by_email).return_value = user
-    #
-    #     with self.app.container.user_repo.override(user_repo_mock):
-    #         resp = self.call_web_incident_api(token, body)
-    #
-    #     self.assertEqual(resp.status_code, 401)
-    #     resp_data = json.loads(resp.get_data())
-    #     self.assertEqual(resp_data['code'], 401)
-    #     self.assertEqual(resp_data['message'], 'Unauthorized: User does not belong to your client.')
-    #
-    # def test_web_incident_success(self) -> None:
-    #     token = gen_token(
-    #         user_id=cast(str, self.faker.uuid4()),
-    #         client_id=cast(str, self.faker.uuid4()),
-    #         role=Role.ADMIN,
-    #         assigned=True,
-    #     )
-    #     user = User(
-    #         id=cast(str, self.faker.uuid4()),
-    #         client_id=token['cid'],
-    #         name=self.faker.name(),
-    #         email=self.faker.email(),
-    #     )
-    #     body = {
-    #         'email': user.email,
-    #         'name': self.faker.word(),
-    #         'description': self.faker.sentence(),
-    #     }
-    #
-    #     user_repo_mock = Mock(UserRepository)
-    #     incident_repo_mock = Mock(IncidentRepository)
-    #
-    #     cast(Mock, user_repo_mock.find_by_email).return_value = user
-    #
-    #     with (
-    #         self.app.container.user_repo.override(user_repo_mock),
-    #         self.app.container.incident_repo.override(incident_repo_mock),
-    #     ):
-    #         resp = self.call_web_incident_api(token, body)
-    #
-    #     self.assertEqual(resp.status_code, 201)
-    #     resp_data = json.loads(resp.get_data())
-    #     self.assertEqual(resp_data['name'], body['name'])
-    #     self.assertEqual(resp_data['channel'], Channel.WEB.value)
-    #     self.assertEqual(resp_data['reported_by'], user.id)
-    #     self.assertEqual(resp_data['created_by'], token['sub'])
+    def test_web_incident_no_token(self) -> None:
+        resp = self.call_web_incident_api(None, None)
+        self.assertEqual(resp.status_code, 401)
+        resp_data = json.loads(resp.get_data())
+        self.assertEqual(resp_data, {'code': 401, 'message': 'Token is missing'})
+
+    @parametrize(
+        'role',
+        [
+            (Role.USER,),
+            (Role.ANALYST,),
+        ],
+    )
+    def test_web_incident_invalid_role(self, role: Role) -> None:
+        token = gen_token(
+            user_id=cast(str, self.faker.uuid4()),
+            client_id=cast(str, self.faker.uuid4()),
+            role=role,
+            assigned=True,
+        )
+        resp = self.call_web_incident_api(token, {})
+        self.assertEqual(resp.status_code, 403)
+        resp_data = json.loads(resp.get_data())
+        self.assertEqual(resp_data, {'code': 403, 'message': 'Forbidden: You do not have access to this resource.'})
+
+    def test_web_incident_no_client(self) -> None:
+        token = gen_token(
+            user_id=cast(str, self.faker.uuid4()),
+            client_id=None,
+            role=Role.ADMIN,
+            assigned=True,
+        )
+        resp = self.call_web_incident_api(token, {})
+        self.assertEqual(resp.status_code, 401)
+        resp_data = json.loads(resp.get_data())
+        self.assertEqual(resp_data, {'code': 401, 'message': 'Unauthorized: You do not belong to any client.'})
+
+    def test_web_incident_invalid_body(self) -> None:
+        token = gen_token(
+            user_id=cast(str, self.faker.uuid4()),
+            client_id=cast(str, self.faker.uuid4()),
+            role=Role.ADMIN,
+            assigned=True,
+        )
+        resp = self.call_web_incident_api(token, {'name': 'test'})
+        self.assertEqual(resp.status_code, 400)
+        resp_data = json.loads(resp.get_data())
+        self.assertEqual(resp_data['code'], 400)
+
+    def test_web_incident_user_not_found(self) -> None:
+        token = gen_token(
+            user_id=cast(str, self.faker.uuid4()),
+            client_id=cast(str, self.faker.uuid4()),
+            role=Role.ADMIN,
+            assigned=True,
+        )
+        body = {
+            'email': self.faker.email(),
+            'name': self.faker.word(),
+            'description': self.faker.sentence(),
+        }
+
+        user_repo_mock = Mock(UserRepository)
+        cast(Mock, user_repo_mock.find_by_email).return_value = None
+
+        with self.app.container.user_repo.override(user_repo_mock):
+            resp = self.call_web_incident_api(token, body)
+
+        self.assertEqual(resp.status_code, 404)
+        resp_data = json.loads(resp.get_data())
+        self.assertEqual(resp_data['code'], 404)
+        self.assertEqual(resp_data['message'], 'Invalid value for email: User does not exist.')
+
+    def test_web_incident_user_not_belonging_to_client(self) -> None:
+        token = gen_token(
+            user_id=cast(str, self.faker.uuid4()),
+            client_id=cast(str, self.faker.uuid4()),
+            role=Role.ADMIN,
+            assigned=True,
+        )
+        user = User(
+            id=cast(str, self.faker.uuid4()),
+            client_id=cast(str, self.faker.uuid4()),  # Different client_id than token
+            name=self.faker.name(),
+            email=self.faker.email(),
+        )
+        body = {
+            'email': user.email,
+            'name': self.faker.word(),
+            'description': self.faker.sentence(),
+        }
+
+        user_repo_mock = Mock(UserRepository)
+        cast(Mock, user_repo_mock.find_by_email).return_value = user
+
+        with self.app.container.user_repo.override(user_repo_mock):
+            resp = self.call_web_incident_api(token, body)
+
+        self.assertEqual(resp.status_code, 401)
+        resp_data = json.loads(resp.get_data())
+        self.assertEqual(resp_data['code'], 401)
+        self.assertEqual(resp_data['message'], 'Unauthorized: User does not belong to your client.')
+
+    def test_web_incident_success(self) -> None:
+        token = gen_token(
+            user_id=cast(str, self.faker.uuid4()),
+            client_id=cast(str, self.faker.uuid4()),
+            role=Role.ADMIN,
+            assigned=True,
+        )
+        user = User(
+            id=cast(str, self.faker.uuid4()),
+            client_id=token['cid'],
+            name=self.faker.name(),
+            email=self.faker.email(),
+        )
+        body = {
+            'email': user.email,
+            'name': self.faker.word(),
+            'description': self.faker.sentence(),
+        }
+
+        user_repo_mock = Mock(UserRepository)
+        incident_repo_mock = Mock(IncidentRepository)
+
+        cast(Mock, user_repo_mock.find_by_email).return_value = user
+
+        # Mocking incident_repo to return a valid IncidentResponse object
+        incident_response = IncidentResponse(
+            id=cast(str, self.faker.uuid4()),
+            client_id=token['cid'],
+            name=body['name'],
+            channel=Channel.WEB.value,
+            reported_by=user.id,
+            created_by=token['sub'],
+            assigned_to=token['sub'],
+        )
+        cast(Mock, incident_repo_mock.create).return_value = incident_response
+
+        with (
+            self.app.container.user_repo.override(user_repo_mock),
+            self.app.container.incident_repo.override(incident_repo_mock),
+        ):
+            resp = self.call_web_incident_api(token, body)
+
+        self.assertEqual(resp.status_code, 201)
+        resp_data = json.loads(resp.get_data())
+        self.assertEqual(resp_data['name'], body['name'])
+        self.assertEqual(resp_data['channel'], Channel.WEB.value)
+        self.assertEqual(resp_data['reported_by'], user.id)
+        self.assertEqual(resp_data['created_by'], token['sub'])
+
