7960244030ede0693f83952eb9df2bd7c9c71979
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-11-01 16:49:51 -0500

    Se agrega variable de ambiente de service client

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 
 from flask import Flask
-from gcp_microservice_utils import GcpAuthToken, setup_apigateway, setup_cloud_logging, setup_cloud_trace
+from gcp_microservice_utils import setup_apigateway, setup_cloud_logging, setup_cloud_trace
 
 from blueprints import BlueprintHealth, BlueprintIncident
 from containers import Container
+from environment import configure_environment_variables
 
 
 class FlaskMicroservice(Flask):
@@ -18,27 +19,7 @@ def create_app() -> FlaskMicroservice:
     app = FlaskMicroservice(__name__)
     app.container = Container()
 
-    if 'USER_SVC_URL' in os.environ:  # pragma: no cover
-        app.container.config.svc.user.url.from_env('USER_SVC_URL')
-
-        if 'USER_SVC_TOKEN' in os.environ:
-            app.container.config.svc.user.token_provider.from_value(
-                type('TokenProvider', (object,), {'get_token': lambda: os.environ['USER_SVC_TOKEN']})
-            )
-        elif 'USE_CLOUD_TOKEN_PROVIDER' in os.environ:
-            app.container.config.svc.user.token_provider.from_value(GcpAuthToken(os.environ['USER_SVC_URL']))
-
-    if 'INCIDENTMODIFY_SVC_URL' in os.environ:  # pragma: no cover
-        app.container.config.svc.incidentmodify.url.from_env('INCIDENTMODIFY_SVC_URL')
-
-        if 'INCIDENTMODIFY_SVC_TOKEN' in os.environ:
-            app.container.config.svc.incidentmodify.token_provider.from_value(
-                type('TokenProvider', (object,), {'get_token': lambda: os.environ['INCIDENTMODIFY_SVC_TOKEN']})
-            )
-        elif 'USE_CLOUD_TOKEN_PROVIDER' in os.environ:
-            app.container.config.svc.incidentmodify.token_provider.from_value(
-                GcpAuthToken(os.environ['INCIDENTMODIFY_SVC_URL'])
-            )
+    configure_environment_variables(app.container)
 
     if os.getenv('ENABLE_CLOUD_TRACE') == '1':  # pragma: no cover
         setup_cloud_trace(app)
